pca fix, mean vector rotation

Exercise_1.3/pca.py
@@ -3,7 +3,7 @@ import glob
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy import misc
-from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
 
 
 def pca(data, k):
@@ -17,13 +17,14 @@ def pca(data, k):
     n = data.shape[1]
     m = data.shape[0]
 
-    mean_vector = [np.mean(x) for x in data]
+    mean_vector = [np.mean(x) for x in data.T]
     X = []
     for i in range(len(data)):
         X.append((data[i, :] - mean_vector[i]))
     X = np.array(X)
 
-    cov = (1/(n-1)) * np.matmul(X.T, X)
+    # cov = (1/(n-1)) * np.matmul(X.T, X)
+    cov = np.cov(X)
     eigenvalues, eigenvectors = np.linalg.eig(cov)
     return eigenvectors[:, :k], eigenvalues[:k]
 
@@ -48,6 +49,7 @@ def normalized_linear_combination(vecs, weights):
     '''
 
     #TODO: Your implementation goes here
+    return np.array(np.dot(vecs, weights))
 
 def load_dataset_from_folder(dir):
     '''
@@ -81,13 +83,25 @@ def load_dataset_from_folder(dir):
 '''
 
 
-#TODO: Your implementation goes here
-k = 3
-# data, datashape = load_dataset_from_folder('data/')
+def linear_combination(data, weighs):
+    lin = []
+    for factors in data:
+        sum = 0
+        for i in range(len(factors)):
+            sum += factors[i] * weighs[i]
+        lin.append(sum)
+    return lin
 
-data = np.array(([3, 2], [2, 3]), dtype='float')
+
+k = 3
+#data, datashape = load_dataset_from_folder('data/')
+
+data = np.array(([3, 2, 1], [2, 3, 1], [1, 2, 3]), dtype='float')
 
 eigenvectors, eigenvalues = pca(data, k)
 print(f"Eigenvectors: {eigenvectors}")
 print(f"Eigenvalues: {eigenvalues}")
+
+p = normalized_linear_combination(eigenvectors, eigenvalues)
+print(f"Normalized linear compination: {p}")
 