deaa413bb7f317ca72f6222037339a076e7e1a3c
carlo <carloattardi15@gmail.com>
2019-11-01 15:55:02 +0100

    trys

--- Exercise_1.4/enhance.py
+++ Exercise_1.4/enhance.py
@@ -1,9 +1,18 @@
 import numpy as np
+import numpy.fft as fft
+from scipy.ndimage import gaussian_filter
 import matplotlib.image as mpimg 
 import matplotlib.pyplot as plt 
 
 
-  
+#read the image
 img=mpimg.imread('asguard2.png')
+#plot the image
 imgplot = plt.imshow(img)
+#print
+plt.show()
+
+imgTransform = fft.fft2(img)
+
+imgplot = plt.imshow(imgTransform)
 plt.show()
