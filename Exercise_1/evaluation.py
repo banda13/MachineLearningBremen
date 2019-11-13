08461bc2eede6e0c57625228c7e96dd4f953f609
Fabrizio <bigfabbro93@gmail.com>
2019-11-13 16:19:48 +0100

    Finished ex. 1

--- Exercise_1/evaluation.py
+++ Exercise_1/evaluation.py
@@ -16,11 +16,10 @@ class Evaluation:
         """ This function does the stratification of the data. More precisely,
             it uses an array of counters, with dimension equal to the number of
             classes of the dataset,  to organize the data in a way such that each folder
-            will have the same number of instances of each class. This is realized by coping
+            keeps the same ratio of instances of each class. This is realized by coping
             the indices array and visiting it sequentially until an element of a class with
-            the corresponding counter less than necessary quantity (=n_elem_class) is found.
-            At this point the index of this element is added at the result array and is cancelled
-            in the copied array.
+            the corresponding counter higher than zero is found. At this point the index of
+            this element is added at the result array and is cancelled in the copied array.
 
             Parameters
             ----------
@@ -33,37 +32,44 @@ class Evaluation:
 
             y : array-like, shape (n_samples)
                 labels of the data
+
+            Returns
+            -------
+
+            new_array : array-like, shape (n_samples)
+                Array of indices corresponding to the instaces of dataset stratificated.
+
             """
 
         new_array = []
         k = 0
-        cl_tp = 0
         class_array, counts = np.unique(y, return_counts=True)
-        counts = np.floor_divide(counts,n_folds)
-        counters_array = np.copy(counts)
+        counters_array = np.copy(np.floor_divide(counts,n_folds))
         temp_array = np.copy(ind_array)
-        n_elem_class = (len(data[0]) / n_folds) / len(class_array)
         for i in range(n_folds):
             while k < len(temp_array):
-                for j in range(len(class_array)):
-                    if y[temp_array[k]] == class_array[j]:
-                        cl_tp = j
-                        break
-
+                # pick the class of the current instance
+                cl_tp = y[temp_array[k]]
+                # check if the current instance can be inserted in the array
                 if counters_array[cl_tp] > 0:
                     new_array.append(temp_array[k])
                     temp_array = np.delete(temp_array,k)
                     counters_array[cl_tp] -= 1
+                    # decrementing the number of instances of the class
+                    counts[cl_tp] -= 1
                 else:
                     k += 1
 
                 if np.sum(counters_array) == 0:
-                    counters_array = np.copy(counts)
+                    if i != 9:
+                        # updating the ratio of the remaining instances
+                        # necessary in case of not equally distributed class
+                        counters_array = np.copy(np.floor_divide(counts,n_folds - i - 1))
                     k = 0
                     break
         return new_array
 
-    def generate_cv_pairs(self, n_samples, n_folds=5, n_rep=1, rand=False,
+    def generate_cv_pairs(self, n_samples, n_folds=5, rep=1, random=False,
                           y=None):
         """ Train and test pairs according to k-fold cross validation
 
@@ -100,21 +106,15 @@ class Evaluation:
 
         indices_array = np.arange(0,n_samples)
         cv_splits = []
-        working_data = ()
         folder_dimension = n_samples/n_folds
-        if rand == False:
-            n_rep = 1
+        if not random:
+            rep = 1
 
-        for i in range(n_rep):
-            if rand == True:
+        for i in range(rep):
+            if random:
                 np.random.shuffle(indices_array)
-                if y is not None:
-                    indices_array = self.stratification(n_folds, indices_array, y)
-                else:
-                    working_data = data
-            else:
-                working_data = data
-
+            if y is not None:
+                indices_array = self.stratification(n_folds, indices_array, y)
             ini = 0
             end = folder_dimension
             actual_shift = 0
@@ -167,7 +167,7 @@ class Evaluation:
         performance : float
             The average metric value across train-test-pairs
         """
-        ### YOUR IMPLEMENTATION GOES HERE ###
+
         sum = 0
         X_train = []
         Y_train = []
@@ -182,7 +182,6 @@ class Evaluation:
                     X_test.append((X[train_test_pairs[i][1][j-len(train_test_pairs[i][0])]]))
                     Y_test.append((y[train_test_pairs[i][1][j-len(train_test_pairs[i][0])]]))
             sum += classifier(X_train,X_test,Y_train,Y_test)
-
         return sum/len(train_test_pairs)
 
 
@@ -216,16 +215,37 @@ class Evaluation:
         acc = bbc.score(X_test, y_test)
         return acc
 
-if __name__ == '__main__':
-    # Instance of the Evaluation class
 
-    label_pairs = []
-    eval = Evaluation()
+if __name__ == '__main__':
+
+    """
+    Configuration parameters 
+    ------------------------
+       
+       n_folds = int
+            number of folder in which the dataset is divided (e.g. k in k-fold cross validation)
+       
+       n_rep = int
+            number of repetition of the k-fold cross validation. If rand == false, n_rep will be forced to 1.
+       
+       rand = boolean
+            if true, randomization is applied to k-fold cross validation
+       
+       strat = boolean
+            if true, stratification is applied to k-fold cross validation
+            
+    """
+    n_folds = 10
     n_rep = 10
     rand = True
+    strat = True
     data = load_iris(True)
+
+    eval = Evaluation()
     n_samples = len(data[0])
-    n_folds = 10
+    y = None
+    if strat:
+        y = data[1]
     cv_pairs = eval.generate_cv_pairs(n_samples, n_folds, n_rep, rand, data[1])
-    print(eval.apply_cv(data[0],data[1],cv_pairs,eval.black_box_classifier))
-    #print(cv_pairs)
+    print("Average metric value: %s" % eval.apply_cv(data[0],data[1],cv_pairs,eval.black_box_classifier))
+
