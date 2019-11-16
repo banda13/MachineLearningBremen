fdce937c26e3019150d610616d794f11ed112462
Szabó András <szabag@gmail.com>
2019-11-16 19:41:17 +0100

    Exercise 2.1 completed: c index

---
+++ Exercise_2_1/c_index.py
@@ -0,0 +1,45 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from Exercise_2_1.kmeans import KMeans
+
+
+def calculate_C_index(X, clusters):
+    P = [(X[i], X[j]) for i in range(len(X)) for j in range(i + 1, len(X))]
+    Q = [(X[i], X[j]) for i in range(len(X)) for j in range(i + 1, len(X)) if clusters[i] == clusters[j]]
+    Scl = sum([np.linalg.norm(np.array(i[0]) - np.array(i[1])) for i in Q])
+    q = len(Q)
+    P.sort(key=lambda x: np.linalg.norm(np.array(x[0]) - np.array(x[1])))
+    Smin = sum([np.linalg.norm(np.array(i[0]) - np.array(i[1])) for i in P[:q]])
+    Smax = sum([np.linalg.norm(np.array(i[0]) - np.array(i[1])) for i in P[(len(P) - q):]])
+    return (Scl - Smin) / (Smax - Smin)
+
+
+def plot_results(min_c_values, avg_c_values):
+    plt.plot(min_c_values, 'r')
+    plt.plot(avg_c_values, 'b')
+    plt.show()
+
+
+if __name__ == '__main__':
+    X = np.genfromtxt('cluster_dataset2d.txt', delimiter=',')
+    min_c_values = []
+    avg_c_values = []
+
+    for k in range(2, 10):
+        c_idxs = np.array([])
+        for i in range(50):
+            while True:
+                try:
+                    c = KMeans(k)
+                    c.fit(X)
+                    clusters = c.predict(X)
+                    c_idxs = np.append(c_idxs, calculate_C_index(X, clusters))
+                    break
+                except Exception as e:
+                    pass  # print(e)
+
+        print('k: {} -> min: {}, avg: {}'.format(k, np.min(c_idxs), c_idxs.mean()))
+        min_c_values.append(np.min(c_idxs))
+        avg_c_values.append(c_idxs.mean())
+    plot_results(min_c_values, avg_c_values)
