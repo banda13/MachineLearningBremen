a1e0822da928903b5afec00fda0900bdd896550d
Fabrizio <bigfabbro93@gmail.com>
2019-11-16 12:48:47 +0100

    first exercise works

--- Exercise_2_1/kmeans.py
+++ Exercise_2_1/kmeans.py
@@ -7,7 +7,7 @@ import random
 
 class KMeans:
     
-    def __init__(self, k=1, epochs=1):
+    def __init__(self, k=1, epochs=6):
         """ Init stuff
 
         Parameters
@@ -22,6 +22,9 @@ class KMeans:
         """
         self.k = k
         self. epochs = epochs
+        self.centroids = []
+
+
 
     def fit(self, X):
         """Compute k-means clustering.
@@ -30,11 +33,38 @@ class KMeans:
         X : array-like or sparse matrix, shape=(n_samples, n_features)
             Training instances to cluster.
         """
-
-        centers = []
+        exclude = []
         # Generate the first k centroid randomly
-        for i in range(self.k):
-            centers = np.append(centers, random.choice([i for i in range(len(X)) if i not in centers]))
+        for j in range(self.k):
+            rand = random.choice([i for i in range(len(X)) if i not in exclude])
+            self.centroids.append(X[rand])
+            exclude.append(rand)
+        labels = self.predict(X)
+        self.plotResult(labels)
+        for i in range(self.epochs):
+            sum_X = [0] * self.k
+            sum_Y = [0] * self.k
+            mean_X = [0] * self.k
+            mean_Y = [0] * self.k
+            counter = [0] * self.k
+            for j in range(len(labels)):
+                sum_X[labels[j]] += X[j][0]
+                sum_Y[labels[j]] += X[j][1]
+                counter[labels[j]] +=1
+            for l in range(self.k):
+                mean_X[l] = sum_X[l]/counter[l]
+                mean_Y[l] = sum_Y[l]/counter[l]
+                self.centroids[l] = [mean_X[l],mean_Y[l]]
+            labels = self.predict(X)
+            self.plotResult(labels)
+        print(self.centroids)
+
+
+
+
+
+
+
 
 
     def predict(self, X):
@@ -51,14 +81,30 @@ class KMeans:
         labels : array, shape [n_samples,]
             Index of the cluster each sample belongs to.
         """
-        ### YOUR IMPLEMENTATION GOES HERE ###
-        
+        labels = []
+        for observation in X:
+            min = 999999
+            centr_min = None
+            for j in range(len(self.centroids)):
+                distance_centr = np.sqrt(np.power(observation[0]-self.centroids[j][0], 2) + np.power(observation[1]-self.centroids[j][1],2))
+                if distance_centr < min:
+                    min = distance_centr
+                    centr_min = j
+            labels.append(centr_min)
+        return labels
+
+    def plotResult(self, labels):
+        scatter = plt.scatter([item[0] for item in X], [item[1] for item in X], s=50, c=[item for item in labels])
+        plt.scatter([item[0] for item in self.centroids], [item[1] for item in self.centroids], s=100, c='g', marker='*')
+        plt.xlabel("Feature 1")
+        plt.ylabel("Feature 2")
+        plt.legend(*scatter.legend_elements(), loc="upper right", title="Cluster")
+        plt.axis([0, 40, 0, 40])
+        plt.show()
 
 if __name__ == '__main__':
     # Load data
     X = np.genfromtxt('cluster_dataset2d.txt', delimiter=',')
-
     # Instance of the Kmeans class
-    c = KMeans(5)
+    c = KMeans(6)
     c.fit(X)
-    c.predict(X)
