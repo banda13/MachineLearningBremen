1f07f5c93aa7d9b8ff046c6ea72477cac41a8245
Szabó András <szabag@gmail.com>
2019-12-13 14:27:41 +0100

    fix & format for 4.3

--- Exercise_4_3/bayesian_optimization.py
+++ Exercise_4_3/bayesian_optimization.py
@@ -7,6 +7,7 @@ from sklearn.gaussian_process import GaussianProcessRegressor
 from sklearn.gaussian_process.kernels import RBF
 from sklearn.utils import check_random_state
 
+
 class BayesianOptimizer:
     """ Bayesian Optimizer, this class is already complete! """
 
@@ -91,7 +92,6 @@ class UpperConfidenceBound:
         """
         self.k = k
 
-
     def __call__(self, x, model):
         """__call__: This method makes any object from the class callable like
            a regular function.
@@ -129,6 +129,7 @@ class ExpectedImprovement:
 
         return ei
 
+
 class ProbabilityImprovement:
 
     def __init__(self, X, xi=0.01):
@@ -147,6 +148,7 @@ class ProbabilityImprovement:
 
         return pi
 
+
 def f(x):
     """ The objective function to optimize. Note: Usally this function is not
         available in analytic form, when bayesian optimization is applied. Note
@@ -157,16 +159,16 @@ def f(x):
     x:
         The point(s) to evaluate the objective function at.
     """
-    return (1.0 - np.tanh(x**2)) * np.sin(x*6) + np.random.normal(loc=0.0,
-                                                                  scale=0.02)
+    return (1.0 - np.tanh(x ** 2)) * np.sin(x * 6) + np.random.normal(loc=0.0,
+                                                                      scale=0.02)
 
 
 if __name__ == "__main__":
 
     # set the parameters and execute the bayesian optimization
-    iter = 50
+    iter = 100
     bounds = np.array([-1, 1])
-    optimizer = BayesianOptimizer(UpperConfidenceBound(10))
+    optimizer = BayesianOptimizer(UpperConfidenceBound(4))
     for i in range(iter):
         X = optimizer.get_next_query_point(bounds)
         Y = f(X)
@@ -183,8 +185,8 @@ if __name__ == "__main__":
     plt.plot(x, y, 'r:', label='f(x)')
 
     plt.fill(np.concatenate([x, x[::-1]]),
-             np.concatenate([y_pred - 1.9600 * sigma,
-                             (y_pred + 1.9600 * sigma)[::-1]]),
+             np.concatenate([y_pred - 1.9500 * sigma,
+                             (y_pred + 1.9500 * sigma)[::-1]]),
              alpha=.5, fc='b', ec='None', label='95% confidence interval')
 
     plt.xlabel('$x$')
