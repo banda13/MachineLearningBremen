266d94cc3eb1f430ace31a43c065294c0606d38d
charlesss <carloattardi@gmail.com>
2020-01-25 20:25:09 +0100

    second try of two_headed_model.py

--- Exercise_6_2/two_headed_model.py
+++ Exercise_6_2/two_headed_model.py
@@ -41,49 +41,67 @@ def neural_net(x):
     layer_2 = tf.add(tf.matmul(layer_1, weights['h2']), biases['b2'])
     layer_2 = tf.nn.relu(layer_2)  # activation
     # output layer
-    out_layer = tf.matmul(layer_2, weights['out']) + biases['out']
-    mu, sigma = tf.nn.moments(out_layer, [0])
+    #out_layer = tf.matmul(layer_2, weights['out']) + biases['out']
+    #mu, sigma = tf.nn.moments(out_layer, [0])
 
-    #sigma = tf.nn.relu(sigma)
+    mu_h1 = tf.add(tf.matmul(layer_2, weights['mu1']), biases['mu1'])
+    mu_h1 = tf.nn.relu(mu_h1)
 
-    return (out_layer), mu, sigma
+    mu_out = tf.add(tf.matmul(mu_h1, weights['mu2']), biases['mu2'])
+
+    sigma_h1 = tf.add(tf.matmul(layer_2, weights['sigma1']), biases['sigma1'])
+    sigma_h1 = tf.nn.sigmoid(sigma_h1)
+
+    sigma_out = tf.add(tf.matmul(sigma_h1, weights['sigma2']), biases['sigma2'])
+
+    #return (out_layer), mu_h2, sigma_h2
+    return mu_out, sigma_out
 
 
 if __name__ == "__main__":
-    x_train, y_train, x_test, y_test = generate_sine_dataset(17)
+    x_train, y_train, x_test, y_test = generate_sine_dataset(89)
 
     X = tf.placeholder("float", [None, 1])
-    Y = tf.placeholder("float", [None, 1])
+    MU = tf.placeholder("float", [None, 1])
+    SIGMA = tf.placeholder("float", [None, 1])
 
     weights = {
         'h1': tf.Variable(tf.random_normal([1, 20])),  # 4 inputs 10  nodes in h1 layer
         'h2': tf.Variable(tf.random_normal([20, 10])),  # 10 nodes in h2 layer
-        'out': tf.Variable(tf.random_normal([10, 1])),  # 1 ouput label
-        'mu_sigma': tf.Variable(tf.random_normal([1, 2]))
+        #'out': tf.Variable(tf.random_normal([10, 1])),  # 1 ouput label
+        #'mu_sigma': tf.Variable(tf.random_normal([1, 2])),
+        'mu1': tf.Variable(tf.random_normal([10, 20])),
+        'mu2': tf.Variable(tf.random_normal([20, 1])),
+        'sigma1': tf.Variable(tf.random_normal([10, 20])),
+        'sigma2': tf.Variable(tf.random_normal([20, 1])),
     }
     biases = {
         'b1': tf.Variable(tf.random_normal([20])),
         'b2': tf.Variable(tf.random_normal([10])),
-        'out': tf.Variable(tf.random_normal([1])),
-        'mu_sigma': tf.Variable(tf.random_normal([2]))
+        #'out': tf.Variable(tf.random_normal([1])),
+        #'mu_sigma': tf.Variable(tf.random_normal([2])),
+        'mu1' : tf.Variable(tf.random_normal([20])),
+        'mu2': tf.Variable(tf.random_normal([1])),
+        'sigma1': tf.Variable(tf.random_normal([20])),
+        'sigma2': tf.Variable(tf.random_normal([1])),
     }
 
-    Y_hat, mu, sigma = neural_net(X)
-    loss_op = 0.5 / 100 * tf.reduce_sum(tf.log(sigma) + (mu - y_train) ** 2 / sigma)  # loss function
+    mu, sigma = neural_net(X)
+    loss_op = 0.5 / 100 * tf.reduce_sum(tf.log(sigma) + tf.squared_difference(mu, y_train) / sigma)  # loss function
     #loss_op = tf.keras.losses.MSE(y_train,Y_hat)
-    optimizer = tf.train.AdamOptimizer(learning_rate=0.01)  # define optimizer # play around with learning rate
+    optimizer = tf.train.AdamOptimizer(learning_rate=0.001)  # define optimizer # play around with learning rate
     train_op = optimizer.minimize(loss_op)  # minimize loss
     init = tf.global_variables_initializer()
-    epoch = 1000
+    epoch = 5000
 
     with tf.Session() as sess:
         sess.run(init)
         for i in range(0, epoch):
-            sess.run(train_op, feed_dict={X: x_train, Y: y_train})
-            loss = sess.run(loss_op, feed_dict={X: x_train, Y: y_train})
+            sess.run(train_op, feed_dict={X: x_train, MU: np.mean(y_train).reshape(1,1), SIGMA: np.std(y_train).reshape(1,1)})
+            loss = sess.run(loss_op, feed_dict={X: x_train, MU: np.mean(y_train).reshape(1,1), SIGMA: np.std(y_train).reshape(1,1)})
             if (i % 100 == 0):
-                print("epoch no" + str(i), (loss))
-            pred = sess.run(tf.convert_to_tensor(Y_hat), feed_dict={X: x_test})
+                print("epoch no " + str(i), (loss))
+            pred = sess.run([mu,sigma], feed_dict={X: x_test})
 
     # TODO: expected function?, correct interval, scatter
     #       plot mean, variance
@@ -97,7 +115,9 @@ if __name__ == "__main__":
     plt.title("Prediction")
     plt.scatter(x_train.ravel(), y_train.ravel(), label="Training set (noisy)")
     plt.plot(x_test.ravel(), y_test.ravel(), lw=3, label="True function")
-    plt.plot(x_test.ravel(), pred.ravel(), lw=3,
-             label="Prediction")
+    plt.plot(x_test.ravel(), pred[0].ravel(), lw=3,
+             label="Mean")
+    plt.plot(x_test.ravel(), pred[1].ravel(), lw=3,
+             label="Variance")
     plt.legend(loc="best")
     plt.show()
